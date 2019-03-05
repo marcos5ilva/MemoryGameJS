abbcf7975563a14ca380ef9fabd98cfacee6aee0
Marcos Silva <marcos.silva79@gmail.com>
2019-03-04 19:12:20 -0700

    Added 10 more cards

--- app.js
+++ app.js
@@ -11,18 +11,37 @@
  memoryDeck[7]= '<i class="fab fa-react"></i>'; //ReactJS logo
  memoryDeck[8]= '<i class="fab fa-angular"></i>'; //Angular logo
  memoryDeck[9]= '<i class="fab fa-angular"></i>'; //Angular logo
+ memoryDeck[10]= '<i class="fab fa-vuejs"></i>'; //Vue logo
+ memoryDeck[11]= '<i class="fab fa-vuejs"></i>'; //Vue logo
+ memoryDeck[12]= '<i class="fab fa-github"></i>'; //Github logo
+ memoryDeck[13]= '<i class="fab fa-github"></i>'; //Github logo
+ memoryDeck[14]= '<i class="fab fa-python"></i>'; //Python logo
+ memoryDeck[15]= '<i class="fab fa-vuejs"></i>'; //Python logo
+ memoryDeck[16]= '<i class="fab fa-slack-hash"></i>'; //Slack logo
+ memoryDeck[17]= '<i class="fab fa-slack-hash"></i>'; //Slack logo
+ memoryDeck[18]= '<i class="fab fa-stack-overflow"></i>'; //Stack overflow logo
+ memoryDeck[19]= '<i class="fab fa-stack-overflow"></i>'; //Stack overflow logo
 
  
  let drawCard = function(){
      console.log("drawCard called");
     let random = Math.floor(Math.random() * (memoryDeck.length)); 
-    let card = memoryDeck[random];
 
-    //removing the card from the deck
-    memoryDeck.splice(random, 1);
-    console.log(memoryDeck);
-    return card;
+    if(random> memoryDeck.length){
+        random = Math.floor(Math.random() * (memoryDeck.length)); 
+    }
     
+    let card = memoryDeck[random];
+    console.log("card :"+random);
+    //removing the card from the deck
+    
+    if(memoryDeck.length > 3){
+    memoryDeck.splice(random, 1);
+    console.log("memoryDeck size "+memoryDeck.length);
+    console.log("memoDeck :"+memoryDeck);
+    
+    return card;
+    }
 };
 
 let clickCard = function(){
@@ -36,8 +55,6 @@ playGame = function(){
     let finished = false;
     console.log("playGame works!");
     let questionMark = '<i class="fas fa-question-circle"></i>'; //Question mark symbol
-
-   
     
     
 
